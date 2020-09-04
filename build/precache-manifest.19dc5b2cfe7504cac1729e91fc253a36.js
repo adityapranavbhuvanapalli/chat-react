f59764a0fff5b492ba7aaeec85b48613b7bc91eb
Aditya Pranav Bhuvanapalli <pranavba99@gmail.com>
2020-09-04 17:10:36 +0530

    publish 1.0

---
+++ build/precache-manifest.19dc5b2cfe7504cac1729e91fc253a36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2738abd1966a2ebf70c3",
+    "url": "/chat-react/static/css/main.4a075e91.chunk.css"
+  },
+  {
+    "revision": "2738abd1966a2ebf70c3",
+    "url": "/chat-react/static/js/main.29fc3cee.chunk.js"
+  },
+  {
+    "revision": "d0a5c26d9c1e6c6c5d8a",
+    "url": "/chat-react/static/js/runtime~main.e4a160fe.js"
+  },
+  {
+    "revision": "5e0328b4036516454583",
+    "url": "/chat-react/static/js/2.cafa004f.chunk.js"
+  },
+  {
+    "revision": "c36b23454a9990df5c396376770421cc",
+    "url": "/chat-react/index.html"
+  }
+];
